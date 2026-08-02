19b44ff4eb08b579dfc37b93fd2c38c3e779d1f5
Htet Htut <HtetHtut@users.noreply.github.com>
2026-08-02 16:52:52 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        // So basically I need to find each anagram, then if I have a list of that anagram type,
+        // I need to push the unsorted word into the right list
+        // so I will create a list of sorted string to the list of anagrams
+        // then I will just return the values, which should be lists, as a list
+
+        std::unordered_map<std::string, std::vector<std::string>> anagram_map;
+        for (const std::string& og_string : strs) {
+            std::string sorted_string = og_string;
+            std::sort(sorted_string.begin(), sorted_string.end());
+
+            anagram_map[sorted_string].push_back(og_string);
+        }
+        std::vector<std::vector<std::string>> results;
+        for (auto& [sorted_string, string_vec] : anagram_map){
+            results.push_back(std::move(string_vec));
+        }
+        return results;
+    }
+};
