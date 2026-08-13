824badf5ef1d65f321081972a875afd81254d509
Htet Htut <HtetHtut@users.noreply.github.com>
2026-08-12 23:37:00 -0700

    Add: depth-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+
+        return 1 + std::max(maxDepth(root->right), maxDepth(root->left));
+    }
+};
